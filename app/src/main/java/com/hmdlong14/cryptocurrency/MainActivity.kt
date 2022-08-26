7436367694ca71da72b01ecabddb0dbe69fddc25
HMDLong <long.hmd2000@gmail.com>
2022-08-26 10:03:44 +0700

    fix comment pull #1

--- app/src/main/java/com/hmdlong14/cryptocurrency/MainActivity.kt
+++ app/src/main/java/com/hmdlong14/cryptocurrency/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.cryptocurrency
+package com.hmdlong14.cryptocurrency
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hmdlong14.cryptocurrency.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
